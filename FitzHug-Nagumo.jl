7857adca1b2ed8a8299cac7dba32c9ad1dcc1d32
Teslos <toni.ivas@empa.ch>
2024-05-09 10:55:24 +0000

    added example of square network using FHN model and using NN to find connection coefficients

--- FitzHug-Nagumo.jl
+++ FitzHug-Nagumo.jl
@@ -1,4 +1,7 @@
+using Pkg
+Pkg.activate(".")
 using DelimitedFiles
+using Graphs
 
 # adjust the load path for your system
 G = readdlm(joinpath(@__DIR__, "./Norm_G_DTI.txt"),',', Float64, '\n')
@@ -10,11 +13,23 @@ g_weighted = SimpleWeightedDiGraph(G)
 
 # For later use, we extract the edge.weight attributes
 # . is the broadcast operator and gets the attribute :weight for every edge 
-edge_weights = getfield.(collect(edges(g_weighted)), :weight)
+#edge_weights = getfield.(collect(edges(g_weighted)), :weight)
 
 # we promote the g_weighted as directed graph (weights of the edges are included in parameters)
-g_directed = SimpleDiGraph(g_weighted)
+#g_directed = SimpleDiGraph(g_weighted)
 
+function create_graph(N::Int=8, M::Int=10)
+    g = Graphs.grid([N, M])
+    edge_weights = ones(length(edges(g)))
+    g_weighted = SimpleWeightedDiGraph(g)
+    g_directed = SimpleDiGraph(g_weighted)
+    return g_directed, edge_weights
+end
+
+g_directed, edge_weights = create_graph(4, 4)
+println("Number of nodes: ", nv(g_directed))
+println("Number of edges: ", size(edge_weights))
+println("Number of edges: ", ne(g_directed))
 using NetworkDynamics
 
 Base.@propagate_inbounds function fhn_electrical_vertex!(dv, v, edges, p, t)
@@ -37,16 +52,16 @@ odeeleedge = StaticEdge(; f=electrical_edge!, dim=1, coupling=:directed)
 fhn_network! = network_dynamics(odeelevertex, odeeleedge, g_directed)
 
 # Parameter handling
-N = 90 # Number of nodes
+N = nv(g_directed) # Number of nodes
 const ϵ = 0.05 # global variables that are accessed several times should be declared as constants
 const a = 0.5
 const σ = 0.5
 
 
 # Tuple of parameters for nodes and edges
-p = (nothing, σ/ϵ * edge_weights)
+p = (nothing, σ/ϵ * ones(ne(g_directed)))
 #Initial conditions
-x0 = randn(2N)*5
+x0 = randn(2N)
 
 # Solving the ODE
 using OrdinaryDiffEq
@@ -62,22 +77,26 @@ diff_data = Array(sol)
 #using Plots
 #plot(sol, vars=idx_containing(fhn_network!, :u), legend=false, ylim = (-5,5), fmt=:png)
 
-# using the new plotting package GLMakie
-using GLMakie
+# using the new plotting package CairoMakie
+#=
+using CairoMakie
 fig = Figure()
-ax = GLMakie.Axis(fig[1, 1], xlabel = "Time", ylabel = "u", title = "FitzHugh-Nagumo network")
+ax = CairoMakie.Axis(fig[1, 1], xlabel = "Time", ylabel = "u", title = "FitzHugh-Nagumo network")
 t= sol.t
 u = sol(sol.t)[1:N,:]
 for i in 1:N
     lines!(ax, t, u[i,:], color = (:blue, 0.1))
 end
-#fig
+fig
+=#
+using Plots
 using Random
 using Lux
 using Optimization
 using OptimizationOptimJL
 using OptimizationOptimisers
 using ComponentArrays
+using DiffEqFlux
 
 # Learning the coupling strength
 rng = Random.default_rng()
@@ -97,7 +116,7 @@ prob_neuralode = ODEProblem(fhn_network!, x0, tspan, p)
 
 function predict_neuralode(p)
     prob = remake(prob_neuralode, p=p)
-    Array(solve(prob, Tsit5(), saveat=tsteps))
+    Array(solve(prob, Tsit5(), saveat=tsteps,sensealg = ForwardDiffSensitivity()))
 end
 
 
@@ -134,3 +153,4 @@ result_neuralode2 = Optimization.solve(optprob2, Optim.BFGS(; initial_stepnorm =
     callback, allow_f_increases = false)
 
 callback(result_neuralode2.u, loss_neuralode(result_neuralode2.u)...; doplot=true)
+savefig("FitzHug-Nagumo-opt.png")
