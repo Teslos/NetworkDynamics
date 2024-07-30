2974f6aa90570e0fef8a4426ccce8ec1e89aceb8
ivtEMPA <toniivas@gmail.com>
2024-07-30 10:21:43 +0200

    Change the code so that associative memory is working

--- Kuramoto-Izhikevich.jl
+++ Kuramoto-Izhikevich.jl
@@ -6,8 +6,24 @@ using Optimization # for the optimization problem
 using OptimizationOptimisers
 using GLMakie # for the optimization algorithm
 using ScikitLearn
-@sk_import datasets: load_digits
+using FileIO
+using Images
+using Statistics
 
+@sk_import datasets: load_digits
+PL_PERIOD = 50.0
+function load_image_as_array(file_path::String)
+    img_array = zeros(Float64, 8, 8, 2)
+    for i in 1:2
+        file = joinpath(file_path,"digit-$(i).jpg")
+        img = FileIO.load(file)
+        img = Gray.(img)
+        img_array[:,:,i] = ifelse.(convert(Array{Float64,2}, img') .> 0.5, 1.0, -1.0)
+    end
+    return img_array
+end
+
+image_digits = load_image_as_array("digits/")
 # load digits dataset
 digits = load_digits()
 # plot the first 10 digits
@@ -22,28 +38,72 @@ for i in 1:10
     hidexdecorations!(ax)
 end
 GLMakie.save("digits.png",fig_digits, px_per_unit = 4)
+
+function distance_to_target(ϕ::Array{Float64,1}, ϕ_target::Array{Float64,1})
+    # calculate the distance between the target and the current value using cosine similarity
+    return sum(1.0 .- cos.(ϕ-ϕ_target))
+end
+# create the grid graph for the Kuramoto network
 function create_graph(N::Int=8, M::Int=8)
     g = Graphs.grid([N, M])
     edge_weights = ones(length(edges(g)))
     return g, edge_weights
 end
-
+# create the complete graph for the Kuramoto network
+function create_graph_complete(N::Int=5)
+    # create all to all graph
+    g = Graphs.complete_graph(N)
+    edge_weights = ones(length(edges(g)))
+    g_weighted = SimpleDiGraph(g)
+    g_directed = SimpleDiGraph(g_weighted)
+    return g_directed, edge_weights
+end
+function create_barabasi_albert_graph(N::Int=5, k::Int=2)
+    g = barabasi_albert(N, k, is_directed=true)
+    edge_weights = ones(length(edges(g)))
+    g_weighted = SimpleDiGraph(g)
+    g_directed = SimpleDiGraph(g_weighted)
+    return g_directed, edge_weights
+    return g
+end
 function plot_rect_map(N::Int, M::Int, data::Vector{Float64}, ax)
     #ax = GLMakie.Axis(f[1, 1])
     centers_x = 1:N
     centers_y = 1:M
     data = reshape(abs.(data), N, M)
-    GLMakie.heatmap!(ax, centers_x, centers_y, data, colormap = :viridis)
+    GLMakie.heatmap!(ax, centers_x, centers_y, data, colormap = :grays)
+    GLMakie.hidespines!(ax)
+    GLMakie.hidedecorations!(ax)
+    GLMakie.hideydecorations!(ax)
 end
+function plot_phases_distance(N::Int, M::Int, u::Array{Float64,2}, t::Array{Float64,1}, x0::Array{Float64,1} , target::Array{Float64,1}, forcing_period::Float64)
+    f = Figure()
+    ax1 = GLMakie.Axis(f[1, 1], xlabel = "Time", ylabel = "Distance", title = "Distance to target")
+    # Find the index of the value in t that is closest to t0 time
+    for t0 in forcing_period:PL_PERIOD:2500
+        index_closest_to_t = findmin(abs.(t .- t0))[2]
 
+        state_vector_at_t = [mod2pi((u[i,index_closest_to_t]-u[1,index_closest_to_t])-x0[i]) for i in 1:N*M]
+        dis = distance_to_target(vec(state_vector_at_t), target)
+        
+        # scatter plot of the distance
+        GLMakie.scatter!(ax1, t0, dis, color = :blue)
+
+        # record the frames
+    end
+    GLMakie.save("./figs/kuramoto_network_distancephase.png",f, px_per_unit = 4)
+end
 function plot_phases(N::Int, M::Int, u::Array{Float64,2}, t::Array{Float64,1}, x0::Array{Float64,1} ,forcing_period::Float64)
     # Find the index of the value in t that is closest to t0 time
-    for t0 in forcing_period:100:2500
+    for t0 in forcing_period:PL_PERIOD:2500
         f = Figure()
         ax = GLMakie.Axis(f[1, 1], xlabel = "Node", ylabel = "u", title = "Kuramoto network at time $t0")
         index_closest_to_t = findmin(abs.(t .- t0))[2]
+
         state_vector_at_t = [mod2pi((u[i,index_closest_to_t]-u[1,index_closest_to_t])-x0[i]) for i in 1:N*M]
+        
         plot_rect_map(N, M, state_vector_at_t, ax)
+
         # record the frames
         GLMakie.save("./figs/kuramoto_network_diff_phase_t$(t0).png",f, px_per_unit = 4)
     end
@@ -54,8 +114,9 @@ end
 #k = 4 # number of edges per node
 #g = barabasi_albert(N, k)
 N=8; M=8
-g, edge_weights = create_graph(N, M)
-
+g, edge_weights = create_graph_complete(N * M)
+#g, edge_weights = create_graph(N, M)
+#g, edge_weights = create_barabasi_albert_graph(N * M, Int(N * M / 2))
 # create the vector of vectors for the parameter values of the vertices
 ξ = [zeros(ComplexF64, nv(g)) for _ in 1:2]
 ξ[1][1:2:end] .= 0.1+0*im # activating the first part
@@ -64,14 +125,15 @@ g, edge_weights = create_graph(N, M)
 ξ[2][8:8:end] .= 0.1+0*im
 ξ[2][end-7:end] .= 0.1+0*im
 #ξ_0[16:end] .= 0 # activating the end part
-ξ[1] .= reshape(pl_digits[:,:,1,1],(64,1))
-ξ[2] .= reshape(pl_digits[:,:,1,2],(64,1))
+#ξ[1] .= reshape(pl_digits[:,:,1,1],(64,1))
+ξ[1] .= reshape(image_digits[:,:,1],(64,1))
+ξ[2] .= reshape(image_digits[:,:,2],(64,1))
 f = GLMakie.Figure()
 ax = GLMakie.Axis(f[1, 1])
 f
-ξ[1] = normalize(ξ[1])
-ξ[2] = normalize(ξ[2])
-plot_rect_map(N,M, abs.(ξ[2]),ax)
+ξ[1] = normalize.(ξ[1]) # normalize the values
+ξ[2] = normalize.(ξ[2])
+plot_rect_map(N,M, abs.(ξ[1]),ax)
 f
 ω_0 = 1.0 # intrinsic frequency
 ϵ = 100 # coupling strength
@@ -106,9 +168,9 @@ Base.@propagate_inbounds function ki_force_vertex!(dv, v, edges, p, t)
     nothing
 end
 # generating the random values for the parameter value ω_0 of the vertices close to 1
-ξ_0 = copy(ξ[2])
+ξ_0 = copy(ξ[1])
 ζ = 0.1
-ξ_0 = (1-ζ)*ξ[2]+ζ*ξ[1]
+#ξ_0 = (1-ζ)*ξ[1]+ζ*ξ[2]
 #ξ_0[1:6] .= 0.0
 v_pars = ξ_0
 # coupling strength of edges are set to 1/3 @@note: coupling strength of the edges should be given by the
@@ -178,22 +240,23 @@ GLMakie.save("kuramoto_network_diff_phase.png",fig1, px_per_unit = 4)
 using Plots
 Plots.plot(sol.t, u[u_idx[2],:], lw=0.2, label="Node 1")
 plot_phases(N, M, u, t, x0, forcing_period)
-
 # remake the problem with the new parameters
 vpars = zeros(ComplexF64, nv(g))
 #vpars = ξ[2]
-ξ_0 = copy(ξ[2])
-ξ_0[5*8-4] = 0.1 + 0.0im
+ξ_0 = copy(ξ[1])
+ξ_0[5*8:6*8] .= 0.1 + 0.0im
 ξ_0 = normalize(ξ_0)
 vpars = [(real(v), imag(v)) for v in ξ_0]
 parameters = (vpars, e_pars)
 u0 = real(ξ_0)
-optprobr = remake(ode_prob; u0=u0, p=parameters)
+#optprobr = remake(ode_prob; u0=u0, p=parameters)
+# parameters are fixed
+optprobr = remake(ode_prob; u0=u0)
 forcing_period = 0.0
 sol2 = solve(optprobr, Tsit5(), saveat=tspan[1]:0.1:tspan[2])
 u = sol2(sol2.t)[1:N*M,:]
-plot_phases(N, M, abs.(u), sol2.t, zeros(nv(g)), forcing_period)
-
+plot_phases(N, M, u, sol2.t, zeros(nv(g)), forcing_period)
+plot_phases_distance(N, M, u, sol2.t, zeros(nv(g)), angle.(ξ[1]), forcing_period)
 # Learning the diffusion constant
 # using wrapper function to pass tuples as array of parameters
 function nd_wrapper!(dx, x, Σ,t)
