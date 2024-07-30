352d5273f9167ef09e0d1dcd70b9dfeccc9b8edd
ivtEMPA <toniivas@gmail.com>
2024-07-30 18:33:56 +0200

    Adding new test for Kuramoto

--- Kuramoto-Izhikevich-test.jl
+++ Kuramoto-Izhikevich-test.jl
@@ -6,7 +6,24 @@ using Optimization # for the optimization problem
 using OptimizationOptimisers
 using GLMakie # for the optimization algorithm
 using ScikitLearn
+using FileIO
+using Images
+
 @sk_import datasets: load_digits
+
+function load_image_as_array(file_path::String)
+    img_array = zeros(Float64, 8, 8, 2)
+    for i in 1:2
+        file = joinpath(file_path,"digit-$(i).jpg")
+        img = FileIO.load(file)
+        print(img)
+        img = Gray.(img)
+        img_array[:,:,i] = convert(Array{Float64,2}, img)
+    end
+    return img_array
+end
+
+image_digits = load_image_as_array("digits/")
 
 # load digits dataset
 digits = load_digits()
@@ -28,6 +45,15 @@ function create_graph(N::Int=8, M::Int=8)
     return g, edge_weights
 end
 
+function create_graph_complete(N::Int=5)
+    # create all to all graph
+    g = Graphs.complete_graph(N)
+    edge_weights = ones(length(edges(g)))
+    g_weighted = SimpleDiGraph(g)
+    g_directed = SimpleDiGraph(g_weighted)
+    return g_directed, edge_weights
+end
+
 function plot_rect_map(N::Int, M::Int, data::Vector{Float64}, ax)
     #ax = GLMakie.Axis(f[1, 1])
     centers_x = 1:N
@@ -36,13 +62,14 @@ function plot_rect_map(N::Int, M::Int, data::Vector{Float64}, ax)
     GLMakie.heatmap!(ax, centers_x, centers_y, data, colormap = :viridis)
 end
 
-function plot_phases(N::Int, M::Int, u::Array{Float64,2}, t::Array{Float64,1}, x0::Array{Float64,1} ,forcing_period::Float64)
+function plot_phases(N::Int, M::Int, u::Array{Float64,2}, t::Array{Float64,1}, x0::Array{Float64,1} ,forcing_period::Float64, tspan)
     # Find the index of the value in t that is closest to t0 time
-    for t0 in forcing_period:100:2500
+    for t0 in forcing_period:100:tspan[2]
         f = Figure()
         ax = GLMakie.Axis(f[1, 1], xlabel = "Node", ylabel = "u", title = "Kuramoto network at time $t0")
         index_closest_to_t = findmin(abs.(t .- t0))[2]
-        state_vector_at_t = [mod2pi((u[i,index_closest_to_t]-u[1,index_closest_to_t])-x0[i]) for i in 1:N*M]
+        state_vector_at_t = [mod2pi((u[i,index_closest_to_t]-u[1,index_closest_to_t])) for i in 1:N*M]
+        #state_vector_at_t = [mod2pi((u[i,index_closest_to_t]-x0[i]) ) for i in 1:N*M]
         plot_rect_map(N, M, state_vector_at_t, ax)
         # record the frames
         GLMakie.save("./figs/kuramoto_network_diff_phase_t$(t0).png",f, px_per_unit = 4)
@@ -54,23 +81,19 @@ end
 #k = 4 # number of edges per node
 #g = barabasi_albert(N, k)
 N=8; M=8
-g, edge_weights = create_graph(N, M)
+g, edge_weights = create_graph_complete(N*M)
 
 # create the vector of vectors for the parameter values of the vertices
-ξ = [zeros(ComplexF64, nv(g)) for _ in 1:2]
-ξ[1][1:2:end] .= 0.1+0*im # activating the first part
-ξ[2][1:8] .= 0.1+0*im 
-ξ[2][9:8:end] .= 0.1+0*im
-ξ[2][8:8:end] .= 0.1+0*im
-ξ[2][end-7:end] .= 0.1+0*im
-#ξ_0[16:end] .= 0 # activating the end part
-ξ[1] .= reshape(pl_digits[:,:,1,1],(64,1))
-ξ[2] .= reshape(pl_digits[:,:,1,2],(64,1))
+ξ = [zeros(ComplexF64, nv(g)) for _ in 1:10]
+for i in 1:2
+    ξ[i] .= 0.1 .+ im*(normalize(reshape(image_digits[:,:,i],(64,1))))*2*π
+end
+
 f = GLMakie.Figure()
 ax = GLMakie.Axis(f[1, 1])
 f
-ξ[1] = normalize(ξ[1])
-ξ[2] = normalize(ξ[2])
+#ξ[1] = normalize(ξ[1])
+#ξ[2] = normalize(ξ[2])
 plot_rect_map(N,M, abs.(ξ[2]),ax)
 f
 ω_0 = 1.0 # intrinsic frequency
@@ -107,9 +130,10 @@ Base.@propagate_inbounds function ki_force_vertex!(dv, v, edges, p, t)
 end
 # generating the random values for the parameter value ω_0 of the vertices close to 1
 ξ_0 = copy(ξ[2])
-ζ = 0.1
-ξ_0 = (1-ζ)*ξ[2]+ζ*ξ[1]
+
+plot_rect_map(N,M, abs.(ξ_0),ax)
 #ξ_0[1:6] .= 0.0
+f
 v_pars = ξ_0
 # coupling strength of edges are set to 1/3 @@note: coupling strength of the edges should be given by the
 # coupling matrix C_ij = s_ij * exp(ψ_ij*im) where s_ij is the strength of the connection and ψ_ij is the phase difference
@@ -120,7 +144,7 @@ for e in edges(g)
     u,v = src(e), dst(e)
     #c_ij[i] = ϵc*ξ_0[u]*conj(ξ_0[v])
     # sum contribution of all images memorized in ξ
-    c_ij[i] = ϵc/nv(g)*sum([ξ[k][u]*conj(ξ[k][v]) for k in 1:2])
+    c_ij[i] = ϵc/nv(g)*sum([ξ[k][u]*conj(ξ[k][v]) for k in 1:4])
 
     #c_ij[i] = 1.0/3.0 + 1/3*im
     i += 1
@@ -176,23 +200,26 @@ end
 fig1
 GLMakie.save("kuramoto_network_diff_phase.png",fig1, px_per_unit = 4)
 using Plots
-Plots.plot(sol.t, u[u_idx[2],:], lw=0.2, label="Node 1")
-plot_phases(N, M, u, t, x0, forcing_period)
+Plots.plot(sol.t, u[u_idx[22],:], lw=0.2, label="Node 1")
+plot_phases(N, M, u, t, x0, forcing_period, tspan)
 
-# remake the problem with the new parameters
-vpars = zeros(ComplexF64, nv(g))
+# remake the problem with the new vertex parameters
+#vpars = zeros(ComplexF64, nv(g))
 #vpars = ξ[2]
 ξ_0 = copy(ξ[2])
-ξ_0[5*8-4] = 0.1 + 0.0im
+ζ = 0.1
+ξ_0 = (1-ζ)*ξ[2]+ζ*ξ[1]
 ξ_0 = normalize(ξ_0)
-vpars = [(real(v), imag(v)) for v in ξ_0]
-parameters = (vpars, e_pars)
-u0 = real(ξ_0)
-optprobr = remake(ode_prob; u0=u0, p=parameters)
+#vpars = [(real(v), imag(v)) for v in ξ_0]
+parameters = (v_pars, e_pars)
+# input values are given by pattern to recognize
+u0 = imag(ξ_0)
+#optprobr = remake(ode_prob; u0=u0, p=parameters, tspan=tspan)
+optprobr = remake(ode_prob; u0=u0)
 forcing_period = 0.0
 sol2 = solve(optprobr, Tsit5(), saveat=tspan[1]:0.1:tspan[2])
 u = sol2(sol2.t)[1:N*M,:]
-plot_phases(N, M, abs.(u), sol2.t, zeros(nv(g)), forcing_period)
+plot_phases(N, M, abs.(u), sol2.t, zeros(nv(g)), forcing_period, tspan)
 
 # Learning the diffusion constant
 # using wrapper function to pass tuples as array of parameters
